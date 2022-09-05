a9e23c4d2fe8695ebe16f005922d75e287ccc3ee
Dálcio Macuete Garcia <dalciomacuetegarcia@gmail.com>
2022-09-05 17:43:00 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ColorSchemeToggle } from 'components/ColorSchemeToggle/ColorSchemeToggle';
+
+const HomePage = () => (
+  <>
+    <h1>World&apos;s Nations Info</h1>
+    <ColorSchemeToggle />
+  </>
+);
+
+export default HomePage;
